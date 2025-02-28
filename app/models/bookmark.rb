6c048e888ef00ec53a2b766d9a2d8a245166ab79
Ed Audland <edaudland@gmail.com>
2025-02-28 11:12:06 +0000

    Adding root path

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie, uniqueness: { scope: :list }
   validates :comment, length: { minimum: 6 }
+  validates :movie, uniqueness: { scope: :list_id, message: "Already in your list" }
 end
